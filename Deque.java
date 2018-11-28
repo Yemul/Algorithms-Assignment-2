e2f13cd672ad4178ee5204d60085260d88c2ec51
Mengyu Li <limengyuw@gmail.com>
2018-11-28 15:33:30 +1100

    Implement the resize method for the underlying array.

--- Deque.java
+++ Deque.java
@@ -1,22 +1,40 @@
 import java.util.Iterator;
 public class Deque<Item> implements Iterable<Item> {
+    private Item[] A = (Item[]) new Object[1];
+    private int count;
+    private int Front;
+    private int End;
+
     public Deque()
     {
+        count = 0;
+        Front = 0;
+        End = 0;
+    }
 
+    private void resize(int max)
+    {
+        Item[] NewArray = (Item[]) new Object[max];
+        for (int i = 0; i < count; i ++)
+        {
+            NewArray[i] = A[i];
+        }
+        A = NewArray;
     }
 
     public boolean isEmpty()
     {
-
+        return (count == 0);
     }
 
     public int size()
     {
-
+        return count;
     }
 
     public void addFirst(Item item)
     {
+
 
     }
 
