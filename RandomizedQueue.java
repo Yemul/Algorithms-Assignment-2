c01ca1ee37bfdb96da063b011e6e43ce4755cc0d
Mengyu Li <limengyuw@gmail.com>
2018-11-28 16:39:18 +1100

    Setting up the API for RandomizedQueue class.

--- RandomizedQueue.java
+++ RandomizedQueue.java
@@ -1,2 +1,56 @@
-public class RandomizedQueue {
+import java.util.Iterator;
+public class RandomizedQueue<Item> implements Iterable<Item> {
+    public RandomizedQueue()
+    {
+
+    }
+
+    public boolean isEmpty()
+    {
+
+    }
+
+    public int size()
+    {
+
+    }
+
+    public void enqueue()
+    {
+
+    }
+
+    public Item dequeue()
+    {
+
+    }
+
+    public Item sample()
+    {
+
+    }
+
+    public Iterator<Item> iterator()
+    {
+        return new RandomizedArrayIterator();
+    }
+
+    private class RandomizedArrayIterator implements Iterator<Item>
+    {
+        public boolean hasNext()
+        {
+
+        }
+
+        public Item next()
+        {
+
+        }
+
+        public void remove()
+        {
+            throw new java.lang.UnsupportedOperationException("remove is not supported.");
+        }
+    }
+
 }
